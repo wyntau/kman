92b9a4be01519f57c3dc911688146649b3fda151
Treri <treri.liu@gmail.com>
2014-09-02 17:56:09 +0800

    update test

--- test/client/unit/spec/demo.js
+++ test/client/unit/spec/demo.js
@@ -22,7 +22,7 @@ describe('demo suite', function() {
         var authorized = {
             authorized: true
         };
-        $httpBackend.when('GET', '/resource/posts').respond([{
+        $httpBackend.when('GET', '/api/posts').respond([{
             _id: '1',
             content: 'content',
             createdBy: 'author',
